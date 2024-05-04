323562857b7aaaccd5e908d847e4cb747d2aadcd
qwreey <me@qwreey.moe>
2024-05-04 23:55:51 +0900

    Safari warn

--- src/class/browsers/SafariWarn.svelte
+++ src/class/browsers/SafariWarn.svelte
@@ -1,9 +1,17 @@
 {#if (window.navigator.userAgent.match(/[Vv]ersion\/[\.\d]+ *[Ss]afari\/[\.\d]+$/)!==null)}
   <div>
-    <p>사파리는 CSS 렌더링이 다른점이 많아 페이지가 온전하게 보이지 않을 수 있습니다. 더 나은 경험을 위해 Chrome, Firefox 를 이용해주세요.<br>그냥 쓰지 말라면 쓰지 마 (분노)</p>
+    <p>사파리는 CSS 렌더링이 다른점이 많아 페이지가 온전하게 보이지 않을 수 있습니다. 더 나은 경험을 위해 Chrome, Firefox 계열 브라우저를 이용해주세요.</p>
   </div>
 {/if}
 
-<style>
-  
+<style lang="scss">
+  div {
+    top: 20px;
+    position: sticky;
+    backdrop-filter: blur(8px) brightness(50%);
+    -webkit-backdrop-filter: blur(8px) brightness(50%);
+    margin: 22px;
+    padding: 0 22px;
+    border-radius: 16px;
+  }
 </style>
